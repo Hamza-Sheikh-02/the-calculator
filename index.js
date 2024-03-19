3c59851c8a7cdfb6d27ef7774b19a0cd777606a4
Hamza_Sheikh <hamza.sheikh888777@gmail.com>
2024-03-20 00:17:45 +0500

    tweak at the text and added console at the end

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number:", type: "number", name: "firstNumber" },
-    { message: "Enter second number:", type: "number", name: "secondNumber" },
+    { message: "Enter your first number:", type: "number", name: "firstNumber" },
+    { message: "Enter your second number:", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operators to perfrom action",
         type: "list",
@@ -23,3 +23,4 @@ else if (answer.operator === "Multiplication") {
 else if (answer.operator === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
+console.log("The End");
--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number:", type: "number", name: "firstNumber" },
-  { message: "Enter second number:", type: "number", name: "secondNumber" },
+  { message: "Enter your first number:", type: "number", name: "firstNumber" },
+  { message: "Enter your second number:", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perfrom action",
     type: "list",
@@ -23,3 +23,5 @@ if (answer.operator === "Addition") {
 } else if (answer.operator === "Division") {
   console.log(answer.firstNumber / answer.secondNumber)
 }
+
+console.log("The End");
